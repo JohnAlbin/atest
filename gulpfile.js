a9229e1c4d965360f0113cdcd7e16264c0955178
John Albin Wilkins <jwilkins@woodstock.local>
2017-01-05 20:06:05 +0800

    Move gh-pages to docs folder.

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ var options = {};
 // accordingly.
 options.rootPath = {
   project     : __dirname + '/',
-  styleGuide  : __dirname + '/styleguide/',
+  styleGuide  : __dirname + '/docs/',
   theme       : __dirname + '/'
 };
 
